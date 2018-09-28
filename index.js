59bd815b0b8cca1eda7e3a55a0208373cd2240a3
Box <bradycfox@gmail.com>
2018-09-28 15:18:48 -0400

    added check box functionality, but style is a bit funky. Will fix later

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
-// 1. Must be able to see current shopping list
-// 2. Must be able to add items
-// 3. Check and cross off completed items
-// 4. Delete items
-
+/* 
+1.) User can press a switch/checkbox to toggle between displaying all items or displaying only items that are unchecked
+  -
+2.) User can type in a search term and the displayed list will be filtered by item names only containing that search term
+3.) User can edit the title of an item 
+*/
 
 'use strict';
 
-const STORE = [
-    {
-        name: 'apples',
-        checked: false
-    },
-    {
-        name: 'oranges',
-        checked: false
-    },
-    {
-        name: 'milk',
-        checked: true
-    },
-    {
-        name: 'bread',
-        checked: false
-    }
-];
+const STORE = {
+    items: [  
+        {
+            name: 'apples',
+            checked: false
+        },
+        {
+            name: 'oranges',
+            checked: false
+        },
+        {
+            name: 'milk',
+            checked: true
+        },
+        {
+            name: 'bread',
+            checked: false
+        }
+    ],
+    hide: false,
+    searchTerm: ''
+};
 
-function generateItemElement(item, itemIndex, template) {
+function generateItemElement(item, itemIndex) {
     return `
     <li class="js-item-index-element" data-item-index="${itemIndex}">
       <span class="shopping-item js-shopping-item ${item.checked ? 'shopping-item__checked' : ''}">${item.name}</span>
@@ -43,26 +48,28 @@ function generateItemElement(item, itemIndex, template) {
 
 function generateShoppingItemsString(shoppingList) {
     // takes in argument shoppingList
-    console.log('Generating shopping list element');
     const items = shoppingList.map((item, index) => generateItemElement(item, index));
   
     return items.join('');
 }
 
-function renderShoppingList() {
-    console.log('\'renderShoppingList\' ran');
-    // Responsible for displaying shopping list in DOM
-    // Places all shopping items in <ul class="shopping-list js-shopping-list"> 
-    // Joing these together as one long string
-    // insert <li> string inside of the the .js-shopping-list <ul> in the dom
+function renderShoppingList() {    
+    // const shoppingListItemString = generateShoppingItemsString(STORE);
+    // Create a shopping list that updates based on whether hide is true or false.
+
+    let items = Array.from(STORE.items);
+
+    if(STORE.hide === true) { items = Array.from(STORE.items).filter(item => !item.checked); }
     
-    const shoppingListItemString = generateShoppingItemsString(STORE);
-    //places shoppingListItemString into the Shopping List <ul>
+    //call function to attaches the items to an index and then add this to the shopping list html.
+    const shoppingListItemString = generateShoppingItemsString(items); //pass the array to generateShoppingItemsString
+  
     $('.js-shopping-list').html(shoppingListItemString);
 }
 
+
 function addItemShoppingList(newItem) {
-    STORE.push({name: newItem, checked: false});
+    STORE.items.push({name: newItem, checked: false});
 
 
 }
@@ -90,7 +97,17 @@ function getItemIndexFromElement(item) {
 }
 
 function crossCheckedItems(itemIndex){
-    STORE[itemIndex].checked = !STORE[itemIndex].checked; 
+    STORE.items[itemIndex].checked = !STORE.items[itemIndex].checked; 
+}
+
+function toggleHiddenCheckItems() {
+  STORE.hide = !STORE.hide;
+}
+function handleHiddenCheckedItems(){
+  $('#toggle-completed-items').click(function(event) {
+    toggleHiddenCheckItems(STORE.items);
+    renderShoppingList();
+  })
 }
 
 function handleCheckedItems() {
@@ -109,7 +126,7 @@ function handleCheckedItems() {
 }
 
 function deleteItemFromStore (itemIndex) {
-    STORE.splice(itemIndex, 1);
+    STORE.items.splice(itemIndex, 1);
 }
 
 function handleDeletedItems() {
@@ -133,6 +150,7 @@ function handleShoppingList() {
     handleNewItems();
     handleCheckedItems();
     handleDeletedItems();
+    handleHiddenCheckedItems();
 
 
 }
